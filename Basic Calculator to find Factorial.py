ea30983fdee86aa3433e290662a983c611bf96b3
JadhavMehul <44131214+JadhavMehul@users.noreply.github.com>
2018-11-09 23:03:40 +0530

    Factorial
    Basic Calculator to find Factorial of any number in python.

---
+++ Basic Calculator to find Factorial.py
@@ -0,0 +1,9 @@
+a=int(input("enter a number="))
+factorial = 1
+if a <= 0:
+    print("factorial of {} does not exist".format(a))
+
+else:
+    for i in range(1,a+1):
+        factorial=factorial*i
+    print("factorial of {} is {}".format(a,factorial))
